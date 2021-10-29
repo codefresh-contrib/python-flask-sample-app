bb7b08a36d96970da2e6690a3a8b48da9752dbcb
Oleg Sergiyuk <oleg.sergiyuk@gmail.com>
2021-10-29 22:21:28 +0300

    one more edit to commit

--- add_revision.sh
+++ add_revision.sh
@@ -5,3 +5,5 @@ revision=$( sed -n 's/.*Revision \([0-9]\+\).*/\1/p' flaskr/templates/base.html 
 (( revision++ ))
 echo "$revision"
 sed -i.bak '/Revision/ s/ \([0-9]\+\)/ '$revision'/' flaskr/templates/base.html
+
+git commit -a -m "$revision"
