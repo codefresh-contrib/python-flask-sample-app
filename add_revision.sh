209c7e6b38d28229e1aafba0aff4c21fe2eb532e
Oleg Sergiyuk <oleg.sergiyuk@gmail.com>
2021-10-29 22:11:16 +0300

    added commit script

---
+++ add_revision.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# flaskr/templates/base.html
+
+revision=$( sed -n 's/.*Revision \([0-9]\+\).*/\1/p' flaskr/templates/base.html )
+(( revision++ ))
+echo "$revision"
+sed -i.bak '/Revision/ s/ \([0-9]\+\)/ '$revision'/' flaskr/templates/base.html
