e64ac38f24faa397ec4e211807b94e33af4c97c1
Oleg Sergiyuk <oleg.sergiyuk@gmail.com>
2021-10-29 23:00:34 +0300

    3

--- add_revision.sh
+++ add_revision.sh
@@ -4,6 +4,10 @@
 revision=$( sed -n 's/.*Revision \([0-9]\+\).*/\1/p' flaskr/templates/base.html )
 (( revision++ ))
 echo "$revision"
+
+new_revision=revision-0$revision
+git checkout -b ${new_revision}
 sed -i.bak '/Revision/ s/ \([0-9]\+\)/ '$revision'/' flaskr/templates/base.html
 
 git commit -a -m "$revision"
+git push --set-upstream origin ${new_revision}
