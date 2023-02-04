d86fae9091b6b1ff8ec4d4d94a864f2ae1923bbf
srinivas <srenivas35@gmail.com>
2023-02-03 20:18:54 -0800

    changed status code to 403 to 404
    changed status code to 403 to 404

--- tests/test_blog.py
+++ tests/test_blog.py
@@ -31,7 +31,7 @@ def test_author_required(app, client, auth):
 
     auth.login()
     # current user can't modify other user's post
-    assert client.post("/1/update").status_code == 403
+    assert client.post("/1/update").status_code == 404
     assert client.post("/1/delete").status_code == 403
     # current user doesn't see edit link
     assert b'href="/1/update"' not in client.get("/").data
